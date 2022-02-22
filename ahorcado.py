80b08a5026d4fe8bb7bcdbee10f17dfcaddb4335
K4TO <talentounido.kano@gmail.com>
2022-02-22 17:55:24 -0600

    Añadiendo mas palabras para el juego

--- ahorcado.py
+++ ahorcado.py
@@ -80,7 +80,7 @@ IMAGES = ['''
         '''
 ]
 
-WORDS = ['casa','chiconcuautla', 'aguila']
+WORDS = ['casa','botella', 'aguila','cubo','piramide']
 
 def random_word():
     idx = random.randint(0,len(WORDS)-1)
@@ -155,6 +155,7 @@ if __name__ == '__main__':
              ____  _              _ 
        /_//_/ /  / /  /|,//_//  / /
       /`\  / /  /_/  /  /  //_,/_/'''
+   
     bien = '''
 888888b.  8888888 8888888888 888b    888 888     888 8888888888 888b    888 8888888 8888888b.   .d88888b.   .d8888b.  
 888  "88b   888   888        8888b   888 888     888 888        8888b   888   888   888  "Y88b d88P" "Y88b d88P  Y88b 
